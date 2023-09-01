e5b886f7ed33ba3dae88bd712eb8af08330486b6
huning <huning@qq.com>
2023-09-01 11:05:15 +0800

    分页js修改

--- hn-admin-vue2/src/util/paging.js
+++ hn-admin-vue2/src/util/paging.js
@@ -2,44 +2,34 @@ import methods from './methods';
 
 export class Paging {
 	_path = "";
-	_page = 0;
+	_page = 1;
 	_total = 0;
-	_pageSize = 10;
+	_limit = 10;
 	_noMore = false;
 	_list = [];
 	_params = {};
+	_keyword = "";
+	_method = "";
 	
-	constructor(path,params = {}){
+	constructor(path,params = {},method){
 		this._path = path
 		this._params = params
+		this._method = method?method:'get'
 	}
 	
   exec() {
-    // if(this._noMore)return;
-    // this._page++
-    // let obj = { page: this._page, pageSize: this._pageSize }
-		return methods.request(this._path, this._params).then((res) => {
-			console.log(res,7535777777)
-      // if(res.data.code==1){
-      //   if (res.data.data.length > 0 &&this._page==1) this._total = res.data.total;
-      //   this._list = res.data.data;
-      // }
+    let obj = { page: this._page, limit: this._limit }
+		return methods.request(this._path, Object.assign(obj,this._params),this._method).then((res) => {
+			// console.log(res,'paging.js')
+      if(res.code==0){
+        if (this._page==1) this._total = res.total;
+        this._list = res.data;
+				// console.log(this._list,88888)
+				return res.data
+      }
     })
 
 
-		// return new Promise((rs,rj)=>{
-		// 	if(this._noMore)return;
-		// 	this._page++
-		// 	methods.request(this._path,
-    //     Object.assign({ page: this._page, pageSize: this._pageSize }, this._params),
-    //   "get",
-		// 	(res)=>{
-		// 		if(res.success){
-		// 			if (res.data.rows.length < this._pageSize) this._noMore = true;
-		// 			this._list.push(...res.data.rows);
-		// 		}
-		// 	})
-		// })
 	}
 	
 	// reset(){
